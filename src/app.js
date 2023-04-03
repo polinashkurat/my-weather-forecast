d4a25bfe62218cfc14eb68a07b45d36c96ca9141
polinashkurat <polinashkurat63@gmail.com>
2023-04-03 11:52:48 +0200

    Changed the api url

--- src/app.js
+++ src/app.js
@@ -63,7 +63,7 @@ function showForecast(response) {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "1dbf926d3b4417bf379db7043bec1047";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showForecast);
 }
 
